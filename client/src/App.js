6bc9773d2127b39139451cab7bfbaf8da147bd76
sinjeeli <sinjeeli@gmail.com>
2023-08-03 19:49:29 -0400

    updates

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,35 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import Courses from './Courses';
 import CourseDetail from './CourseDetail';
 import CreateCourse from './CreateCourse';
 import UserSignIn from './UserSignIn';
 import UpdateCourse from './UpdateCourse';
-import Header from './Header'; // Import the Header component
+import Header from './Header';
 
 import './App.css';
 
 function App() {
-  
   const user = null; // Replace null with the authenticated user object or null if not authenticated
-
 
   return (
     <Router>
       <div>
         <header>
-        <Header user={user} /> {/* Pass the user prop to the Header component */}
+          <Header user={user} /> {/* Pass the user prop to the Header component */}
         </header>
         <main>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/create">Create Course</Link>
+              </li>
+              {/* Add other navigation links as needed */}
+            </ul>
+          </nav>
           <Routes>
             <Route path="/" element={<Courses />} />
             <Route path="/courses/:id" element={<CourseDetail />} />
