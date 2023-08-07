b26845b68ea4612f4ab5c2e1952b3a23c8b922c0
sinjeeli <sinjeeli@gmail.com>
2023-08-07 03:04:49 -0400

    updates

---
+++ api/migrations/20230807065632-create_courses_table.js
@@ -0,0 +1,43 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Courses', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      title: {
+        type: Sequelize.STRING
+      },
+      description: {
+        type: Sequelize.TEXT
+      },
+      estimatedTime: {
+        type: Sequelize.STRING
+      },
+      materialsNeeded: {
+        type: Sequelize.STRING
+      },
+      userId: {                     // Add this
+        type: Sequelize.INTEGER,    // Specify the type that matches the User's id type
+        references: {
+          model: 'Users',           // 'Users' refers to the table name
+          key: 'id',                // 'id' refers to the column name in Users table
+        }
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Courses');
+  }
+};
